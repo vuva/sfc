429c646cc24c45dcea2054ad77910e3b2998b463
VuVA <vuva@dcn.ssu.ac.kr>
2016-01-15 03:08:36 +0900

    set port

--- sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/SfcScfOfProcessor.java
+++ sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/SfcScfOfProcessor.java
@@ -40,14 +40,14 @@ public class SfcScfOfProcessor {
 
     public SfcScfOfProcessor() {}
 
-   /**
-    * create flows for service function classifier
-    * The function returns true if successful.
-    * The function returns false if unsuccessful.
-    *
-    * @param  scf service function classifier
-    * @return          create result
-    */
+    /**
+     * create flows for service function classifier
+     * The function returns true if successful.
+     * The function returns false if unsuccessful.
+     *
+     * @param  scf service function classifier
+     * @return          create result
+     */
     public boolean createdServiceFunctionClassifier(ServiceFunctionClassifier scf) {
         if (scf == null) {
             LOG.error("createdServiceFunctionClassifier: scf is null\n");
@@ -150,22 +150,19 @@ public class SfcScfOfProcessor {
                 }else{
                     isSubDomain = ace1.isSubDomain();
                 }
-                if (isSubDomain) {
-                    match = new SfcScfMatch()
-                            .setAclMatch(ace.getMatches())
-                            .build();
-                }else{
-                    StringBuffer sb = new StringBuffer();
-                    sb.append(nodeName).append(":");
-                    sb.append(String.valueOf(inPort));
-                    NodeConnectorId port = new NodeConnectorId(sb.toString());
+                match = new SfcScfMatch()
+                        .setAclMatch(ace.getMatches())
+                        .build();
+                StringBuffer sb = new StringBuffer();
+                sb.append(nodeName).append(":");
+                sb.append(String.valueOf(inPort));
+                NodeConnectorId port = new NodeConnectorId(sb.toString());
 
-                    // Match
-                    match = new SfcScfMatch()
-                            .setPortMatch(port)
-                            .setAclMatch(ace.getMatches())
-                            .build();
-                }
+                // Match
+                match = new SfcScfMatch()
+                        .setPortMatch(port)
+                        .setAclMatch(ace.getMatches())
+                        .build();
 
                 // Action
                 Actions actions = ace.getActions();
@@ -200,65 +197,65 @@ public class SfcScfOfProcessor {
 
                 //For H-SFC
                 boolean createResult = isSubDomain?
-                        SfcScfOfUtils.createClassifierOutFlow(nodeName, key.toString(), match, nsh, outPort)
+                        SfcScfOfUtils.createSubDomainClassifierOutFlow(nodeName, key.toString(), match, nsh, outPort)
                         :SfcScfOfUtils.createClassifierOutFlow(nodeName, key.toString(), match, nsh, outPort);
-                if (!createResult) {
-                    LOG.error("createdServiceFunctionClassifier: out flow is null\n");
-                    continue;
-                }
+                        if (!createResult) {
+                            LOG.error("createdServiceFunctionClassifier: out flow is null\n");
+                            continue;
+                        }
 
-                RspName reverseRspName = null;
-                if (path.getRenderedServicePath().endsWith("-Reverse")) {
-                    reverseRspName = new RspName(path.getRenderedServicePath().replaceFirst("-Reverse", ""));
-                } else {
-                    reverseRspName = new RspName(path.getRenderedServicePath() + "-Reverse");
-                }
-
-                SfcNshHeader reverseNsh = SfcNshHeader.getSfcNshHeader(reverseRspName);
-
-                if (reverseNsh == null) {
-                    LOG.debug("createdServiceFunctionClassifier: reverseNsh is null\n");
-                } else {
-                    key = new StringBuffer();
-                    key.append(scf.getName()).append(aclName).append(ruleName).append(".in");
-                    if (!SfcScfOfUtils.createClassifierInFlow(nodeName, key.toString(), reverseNsh, inPort)) {
-                        LOG.error("createdServiceFunctionClassifier: fail to create in flow\n");
-                    }
-
-                    SffName lastSffName = reverseNsh.getSffName();
-                    if (lastSffName != null &&
-                        !reverseNsh.getSffName().equals(sffName)) {
-                        ServiceFunctionForwarder lastSff = SfcProviderServiceForwarderAPI.readServiceFunctionForwarder(lastSffName);
-                        String lastNodeName = SfcOvsUtil.getOpenFlowNodeIdForSff(lastSff);
-                        if (lastNodeName == null) {
-                            LOG.error("createdServiceFunctionClassifier: lastNodeName is null\n");
+                        RspName reverseRspName = null;
+                        if (path.getRenderedServicePath().endsWith("-Reverse")) {
+                            reverseRspName = new RspName(path.getRenderedServicePath().replaceFirst("-Reverse", ""));
                         } else {
-                            LOG.debug("createdServiceFunctionClassifier: relay node is {}\n", lastNodeName);
+                            reverseRspName = new RspName(path.getRenderedServicePath() + "-Reverse");
                         }
-                        outPort = SfcOvsUtil.getVxlanOfPort(lastNodeName);
-                        key = new StringBuffer();
-                        key.append(scf.getName()).append(aclName).append(ruleName).append(".relay");
-                        Ip ip = SfcOvsUtil.getSffVxlanDataLocator(sff);
-                        reverseNsh.setVxlanIpDst(ip.getIp().getIpv4Address());
-                        reverseNsh.setVxlanUdpPort(ip.getPort());
-                        if (!SfcScfOfUtils.createClassifierRelayFlow(lastNodeName, key.toString(), reverseNsh)) {
-                            LOG.error("createdServiceFunctionClassifier: fail to create relay flow\n");
+
+                        SfcNshHeader reverseNsh = SfcNshHeader.getSfcNshHeader(reverseRspName);
+
+                        if (reverseNsh == null) {
+                            LOG.debug("createdServiceFunctionClassifier: reverseNsh is null\n");
+                        } else {
+                            key = new StringBuffer();
+                            key.append(scf.getName()).append(aclName).append(ruleName).append(".in");
+                            if (!SfcScfOfUtils.createClassifierInFlow(nodeName, key.toString(), reverseNsh, inPort)) {
+                                LOG.error("createdServiceFunctionClassifier: fail to create in flow\n");
+                            }
+
+                            SffName lastSffName = reverseNsh.getSffName();
+                            if (lastSffName != null &&
+                                    !reverseNsh.getSffName().equals(sffName)) {
+                                ServiceFunctionForwarder lastSff = SfcProviderServiceForwarderAPI.readServiceFunctionForwarder(lastSffName);
+                                String lastNodeName = SfcOvsUtil.getOpenFlowNodeIdForSff(lastSff);
+                                if (lastNodeName == null) {
+                                    LOG.error("createdServiceFunctionClassifier: lastNodeName is null\n");
+                                } else {
+                                    LOG.debug("createdServiceFunctionClassifier: relay node is {}\n", lastNodeName);
+                                }
+                                outPort = SfcOvsUtil.getVxlanOfPort(lastNodeName);
+                                key = new StringBuffer();
+                                key.append(scf.getName()).append(aclName).append(ruleName).append(".relay");
+                                Ip ip = SfcOvsUtil.getSffVxlanDataLocator(sff);
+                                reverseNsh.setVxlanIpDst(ip.getIp().getIpv4Address());
+                                reverseNsh.setVxlanUdpPort(ip.getPort());
+                                if (!SfcScfOfUtils.createClassifierRelayFlow(lastNodeName, key.toString(), reverseNsh)) {
+                                    LOG.error("createdServiceFunctionClassifier: fail to create relay flow\n");
+                                }
+                            }
                         }
-                    }
-                }
             }
         }
         return true;
     }
 
-   /**
-    * delete flows for service function classifier
-    * The function returns true if successful.
-    * The function returns false if unsuccessful.
-    *
-    * @param  scf service function classifier
-    * @return          delete result
-    */
+    /**
+     * delete flows for service function classifier
+     * The function returns true if successful.
+     * The function returns false if unsuccessful.
+     *
+     * @param  scf service function classifier
+     * @return          delete result
+     */
     public boolean deletedServiceFunctionClassifier(ServiceFunctionClassifier scf) {
         if (scf == null) {
             LOG.error("deletedServiceFunctionClassifier: scf is null\n");
@@ -370,7 +367,7 @@ public class SfcScfOfProcessor {
 
                     SffName lastSffName = reverseNsh.getSffName();
                     if (lastSffName != null &&
-                        !reverseNsh.getSffName().equals(sffName)) {
+                            !reverseNsh.getSffName().equals(sffName)) {
                         ServiceFunctionForwarder lastSff = SfcProviderServiceForwarderAPI.readServiceFunctionForwarder(lastSffName);
                         String lastNodeName = SfcOvsUtil.getOpenFlowNodeIdForSff(lastSff);
                         if (lastNodeName == null) {
